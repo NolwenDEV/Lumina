37b9ba2c6f2c3bb67a0450876a6230977cca6795
Nolwen <Stroobants.Lucas@proton.me>
2025-11-22 19:10:09 +0100

    [📦] Added Teleportation Commands

---
+++ src/be/nolwen/lumina/commands/teleportation/player/TeleportHere.java
@@ -0,0 +1,53 @@
+package be.nolwen.lumina.commands.teleportation.player;
+
+import java.util.Map;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import be.nolwen.lumina.Main;
+import be.nolwen.lumina.utilities.Utils;
+import be.nolwen.lumina.utilities.annotation.registrar.CommandRegistrar;
+
+@CommandRegistrar(NAME = "teleporthere")
+public class TeleportHere implements CommandExecutor {
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] arguments) {
+		if(!command.getName().equalsIgnoreCase("teleporthere")) return true;
+		if(!Main.getInstance().getConfigManager().isTeleportationModule()) return Utils.error(sender, Main.getInstance().getLanguageManager().getFeatureDisabledError());
+		if(!(sender instanceof Player)) return Utils.error(sender, Main.getInstance().getLanguageManager().getPlayerOnlyCommandError());
+		if(arguments.length < 1) return Utils.error(sender, Main.getInstance().getLanguageManager().getTeleportHereUsageError());
+		
+		Player player = (Player) sender;
+		Player target = Bukkit.getPlayerExact(arguments[0]);
+		
+		if(target == player) return Utils.error(sender, Main.getInstance().getLanguageManager().getTeleportationCannotTeleportYourselfError());
+		if(!target.isOnline()) return Utils.error(sender, Main.getInstance().getLanguageManager().getOfflinePlayerError());
+		
+		if(!Main.getInstance().getDataManager().getTeleportRequests().containsKey(target)
+				&& !Main.getInstance().getDataManager().getTeleportRequests().containsKey(player)) {
+			player.sendMessage(Utils.format(Main.getInstance().getLanguageManager().getTeleportationRequestSentMessage(), Map.of("PLAYER", target.getName())));
+			target.sendMessage(Utils.format(Main.getInstance().getLanguageManager().getTeleportationRequestReceivedMessage(),
+					Map.of("PLAYER", player.getName(), "COMMAND", "tpto")
+			));
+			
+			Main.getInstance().getDataManager().getTeleportRequests().put(player, target);
+			Main.getInstance().getDataManager().getTeleportRequests().put(target, player);
+		} else {
+			target.teleport(player.getLocation());
+			
+			player.sendMessage(Utils.format(Main.getInstance().getLanguageManager().getTeleportedAtMessage(), Map.of("PLAYER", target.getName())));
+			target.sendMessage(Utils.format(Main.getInstance().getLanguageManager().getTeleportedToMessage(), Map.of("PLAYER", player.getName())));
+			
+			Main.getInstance().getDataManager().getTeleportRequests().remove(player);
+			Main.getInstance().getDataManager().getTeleportRequests().remove(target);
+		}
+		
+		return true;
+	}
+
+}
