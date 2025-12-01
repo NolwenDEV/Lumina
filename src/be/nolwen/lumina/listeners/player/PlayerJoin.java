4b9c029ef71031dbc6cba5c03c07db1ae9c86c95
Nolwen <Stroobants.Lucas@proton.me>
2025-12-01 18:10:52 +0100

    [📦] Added message to ask player to register or login & Captcha generation

--- src/be/nolwen/lumina/listeners/player/PlayerJoin.java
+++ src/be/nolwen/lumina/listeners/player/PlayerJoin.java
@@ -1,5 +1,6 @@
 package be.nolwen.lumina.listeners.player;
 
+import java.security.SecureRandom;
 import java.util.Map;
 
 import org.bukkit.Bukkit;
@@ -21,7 +22,31 @@ public class PlayerJoin implements Listener {
 				Map.of("PLAYER", event.getPlayer().getName())
 		));
 				
-		Main.getInstance().getSQLManager().executeQuery(Query.UPDATE_PLAYER, event.getPlayer().getUniqueId().toString(), event.getPlayer().getName());	
+		if (Main.getInstance().getConfigManager().isAuthenticationModule()) {
+            Main.getInstance().getSQLManager().executeQuery(Query.RETRIEVE_PLAYER, raw -> {
+            	if(!raw.isEmpty()) {
+            		Map<String, Object> result = raw.get(0);
+            		
+            		if(result.get("CONNECTED") != null && !(Boolean) result.get("CONNECTED")) {
+            			Main.getInstance().getDataManager().getRestrictedPlayers().put(event.getPlayer(), true);
+            			Main.getInstance().getDataManager().getCaptcha().put(event.getPlayer(), String.format("%06d", new SecureRandom().nextInt(1_000_000)));
+            			
+            			if(result.get("PASSWORD") != null) {
+            				event.getPlayer().sendMessage(Utils.format(
+            						Main.getInstance().getLanguageManager().getAuthenticationLoginMessage(),
+            						Map.of("CAPTCHA", Main.getInstance().getDataManager().getCaptcha().get(event.getPlayer()))
+            				));
+            			} else {
+            				event.getPlayer().sendMessage(Utils.format(
+            						Main.getInstance().getLanguageManager().getAuthenticationRegisterMessage(),
+            						Map.of("CAPTCHA", Main.getInstance().getDataManager().getCaptcha().get(event.getPlayer()))
+            				));
+            			}
+            		}
+            	}
+            }, event.getPlayer().getUniqueId().toString(), event.getPlayer().getName(), event.getPlayer().getAddress().getAddress().getHostAddress());
+        }
+		
 		Bukkit.getScheduler().runTaskLater(Main.getInstance(), () -> {
 	        event.getPlayer().setHealth(event.getPlayer().getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue());
 	    }, 5L);
