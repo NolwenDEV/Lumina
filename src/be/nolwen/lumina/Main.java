693bf7d48cc7284d062ced5fa1317a38f1a96e7a
Nolwen <Stroobants.Lucas@proton.me>
2025-12-01 18:09:14 +0100

    [📦] Implemented JDA

--- src/be/nolwen/lumina/Main.java
+++ src/be/nolwen/lumina/Main.java
@@ -11,18 +11,24 @@ import be.nolwen.lumina.utilities.manager.DataManager;
 import be.nolwen.lumina.utilities.manager.SQLManager;
 import be.nolwen.lumina.utilities.manager.configuration.ConfigManager;
 import be.nolwen.lumina.utilities.manager.configuration.DatabaseManager;
+import be.nolwen.lumina.utilities.manager.configuration.DiscordManager;
 import be.nolwen.lumina.utilities.manager.configuration.LanguageManager;
 import be.nolwen.lumina.utilities.manager.updater.Updater;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class Main extends JavaPlugin {
 	
 	private static Main instance;
+	private JDA discordAPI;
 	private Registrar registrar;
 	private ConfigurationLoader configurationLoader;
 	
 	private ConfigManager configManager;
 	private LanguageManager languageManager;
 	private DatabaseManager databaseManager;
+	private DiscordManager discordManager;
 	
 	private SQLManager sqlManager;
 	private DataManager dataManager;
@@ -38,17 +44,29 @@ public class Main extends JavaPlugin {
 		configManager = new ConfigManager();
 		languageManager = new LanguageManager();
 		databaseManager = new DatabaseManager();
+		discordManager = new DiscordManager();
 		
 				 // -------------------- \\
 		
 		configurationLoader.load(configManager);
 		configurationLoader.load(languageManager);
 		configurationLoader.load(databaseManager);
+		configurationLoader.load(discordManager);
 		
 				 // -------------------- \\
 		
 		sqlManager = new SQLManager();
 		dataManager = new DataManager();
+		
+				 // -------------------- \\
+		
+		if (discordManager.isEnabled()) {
+            discordAPI = JDABuilder.createDefault(discordManager.getToken())
+            		.setStatus(discordManager.getOnlineStatus())
+            		.setActivity(discordManager.getActivityStatus())
+            		.enableIntents(GatewayIntent.GUILD_PRESENCES, GatewayIntent.GUILD_MEMBERS, GatewayIntent.MESSAGE_CONTENT)
+            		.build();
+        }
 		
 				 // -------------------- \\
 		
@@ -71,11 +89,13 @@ public class Main extends JavaPlugin {
 	public static Main getInstance() { return instance; }
 	public File getPluginFile() { return this.getFile(); }
 
+	public JDA getDiscordAPI() { return discordAPI; }
 	public ConfigurationLoader getConfigurationLoader() { return configurationLoader; }
 	
 	public ConfigManager getConfigManager() { return configManager; }
 	public LanguageManager getLanguageManager() { return languageManager; }
 	public DatabaseManager getDatabaseManager() { return databaseManager; }
+	public DiscordManager getDiscordManager() { return discordManager; }
 	
 	public SQLManager getSQLManager() { return sqlManager; }
 	public DataManager getDataManager() { return dataManager; }
@@ -86,11 +106,13 @@ public class Main extends JavaPlugin {
 		configurationLoader = new ConfigurationLoader();
 		configManager = new ConfigManager();
 		languageManager = new LanguageManager();
+		discordManager = new DiscordManager();
 		sqlManager = new SQLManager();
 		
 		configurationLoader.load(configManager);
 		configurationLoader.load(languageManager);
 		configurationLoader.load(databaseManager);
+		configurationLoader.load(discordManager);
 	}
 
 }
